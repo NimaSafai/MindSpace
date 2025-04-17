f02094bdfcda57f4d55faadf0cb002db61e45b81
Nima Safai <nima.safai@skf.com>
2025-04-17 15:43:39 +0200

    Fixed navbar to top of page and full width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   background-color: #ffffff;
   padding: 1rem 2rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  z-index: 999;
 `;
 
 const NavLinks = styled.div`
